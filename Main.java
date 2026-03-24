d1c08e5fde5509388cef988dae8446fd338e8f5d
Krishna Datta <136946744+sigatapu@users.noreply.github.com>
2026-03-24 10:51:30 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,19 +1,38 @@
 import java.util.*;
-public class Main {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the value: ");
-        int n = sc.nextInt();
-        System.out.println(count(n));
+
+class Main {
+
+    public static int sum_of_naturalN(int n) {
+        int result = (n * (n + 1)) / 2;
+        return result;
     }
-    public static int count(int n){
+
+    public static int count_digit(int n) {
+        int temp = n;
         int count = 0;
-        int temp = n ;
-        while(temp>0){
-            count = count+1 ;
-            temp = temp/10;
+        while (temp > 0) {
+            count = count + 1;
+            temp = temp / 10;
         }
         return count;
     }
-    
+
+    public static boolean palindrome(int n) {
+        int temp = n;
+        int rev = 0;
+        while(temp>0){
+            int ld = temp%10 ;
+            rev = (rev*10) +  ld;
+            temp = temp/10;
+        }
+        return n==rev;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(palindrome(n));
+
+    }
+
 }
