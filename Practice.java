bb92ec8602cdb4b4b9fb24184cf202683ff3511a
Sigatapu <dattakrishna282@gmail.com>
2026-04-06 20:15:15 +0530

    made few changes

--- Practice.java
+++ Practice.java
@@ -2,15 +2,14 @@ import java.util.ArrayList;
 public class Practice {
     public static void main(String[] args){
 
-        ArrayList<Integer> a = new ArrayList<>();
-        ArrayList<Integer> b = new ArrayList<>();
-        
-        for(int i=10 ; i<=100; i=i+10){ a.add(i);}
-        System.out.println(a);
-        a.add(1,30);
-           
-        System.out.println(a);
-        b=a;
-        System.out.println(b);
+       ArrayList<Integer> a = new ArrayList<>();
+       ArrayList<Integer> b = new ArrayList<>();
+       b.add(92);
+       for(int i=10; i<=100 ; i=i+10){
+           a.add(i);
+       }
+       a.set(0,999);
+//        System.out.println(a.set(0,999));
+        System.out.println(a.get(0));
     }
 }
